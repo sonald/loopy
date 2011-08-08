9bd9312a19b09e1aca02801aaab64b41942f7b61
Sian Cao <sycao@redflag-linux.com>
2011-08-08 17:03:48 +0800

    add mktar.sh

---
+++ mktar.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#set -x
+
+LOOPY_REPO=`pwd`
+VERSION=0.5.4
+
+CLONE_DIR=loopy-$VERSION
+CLONE_REPO=${LOOPY_REPO}/$CLONE_DIR
+TAR_FILE=loopy-${VERSION}.tar.bz2 
+
+rm -rf $CLONE_REPO
+
+git clone $LOOPY_REPO $CLONE_REPO
+tar jcf $TAR_FILE $CLONE_DIR
+
+if [ -d $HOME/rpmbuild/SPECS ]; then
+    echo "copying spec file..."
+    cp $CLONE_REPO/loopy.spec $HOME/rpmbuild/SPECS/
+fi
+
+if [ -d $HOME/rpmbuild/SOURCES ]; then
+    echo "copying source file..."
+    cp $TAR_FILE $HOME/rpmbuild/SOURCES/
+fi
+
